53d4afead2a89f75d4073e83b6c20fd2e7a737df
Bibin Raju <61133690+bbinxx@users.noreply.github.com>
2023-09-28 11:00:50 +0530

    package fix

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 
 const express = require('express') 
-const {readFile,readFileSync} = require('fs');
 const app = express();
 
 
 
 app.get('/',(request,response)=>{
-    response.sendFile('index.htm');
+    response.sendFile(__dirname + '/index.htm');
 
 });
 
